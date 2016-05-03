d6ac6765887bf4a719ce119ebd1887741f44a84b
poprigun <popriguns@gmail.com>
2016-05-03 13:59:38 +0300

    Update ObjectTest.php

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -9,7 +9,7 @@ class ObjectTest extends PHPUnit_Framework_TestCase {
     public function setUp()
     {
         parent::setup();
-        $request = m::mock('Rossedman\Teamwork\Contracts\RequestableInterface');
+        $request = m::mock('Poprigun\Teamwork\Contracts\RequestableInterface');
         $this->object = new ObjectStub($request);
     }
 
@@ -61,7 +61,7 @@ class ObjectTest extends PHPUnit_Framework_TestCase {
 
 }
 
-class ObjectStub extends \Rossedman\Teamwork\AbstractObject {
+class ObjectStub extends \Poprigun\Teamwork\AbstractObject {
 
     public function valid_args($args, $accepted)
     {
